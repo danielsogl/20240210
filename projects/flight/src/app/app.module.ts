787fcebd77bdc82e6ea8512a18ddcb34d9b6bb6d
Daniel Sogl <daniel.sogl@thinktecture.com>
2024-10-02 09:58:32 +0200

    run standalone migration step 2

--- projects/flight/src/app/app.module.ts
+++ projects/flight/src/app/app.module.ts
@@ -4,10 +4,10 @@ import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UiCoreModule } from './shared/ui-core/ui-core.module';
+
 import { HttpClientModule } from '@angular/common/http';
 import { routerFeature } from './shared/logic-router-state';
-import { SharedModule } from './shared/shared.module';
+
 
 
 @NgModule({
@@ -20,10 +20,8 @@ import { SharedModule } from './shared/shared.module';
     HttpClientModule,
     StoreModule.forRoot(),
     EffectsModule.forRoot(),
-    StoreModule.forFeature(routerFeature),
-    UiCoreModule,
-    SharedModule
-  ],
+    StoreModule.forFeature(routerFeature)
+],
   providers: [],
   bootstrap: [AppComponent]
 })
