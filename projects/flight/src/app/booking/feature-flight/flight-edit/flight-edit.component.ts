616c6696402c6fcb0c4d8f545b3d21c1e1a2eb52
Daniel Sogl <daniel.sogl@thinktecture.com>
2024-10-02 13:14:58 +0200

    add typed form example

--- projects/flight/src/app/booking/feature-flight/flight-edit/flight-edit.component.ts
+++ projects/flight/src/app/booking/feature-flight/flight-edit/flight-edit.component.ts
@@ -6,11 +6,24 @@ import {
   OnInit,
   SimpleChanges,
 } from '@angular/core';
-import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  NonNullableFormBuilder,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { routerFeature } from '../../../shared/logic-router-state';
-import { initialFlight } from '../../logic-flight';
+import { Flight, initialFlight } from '../../logic-flight';
+
+interface FlightForm {
+  id: FormControl<Flight['id']>;
+  from: FormControl<Flight['from']>;
+  to: FormControl<Flight['to']>;
+  date: FormControl<Flight['date']>;
+  delayed: FormControl<Flight['delayed']>;
+}
 
 @Component({
   selector: 'app-flight-edit',
@@ -22,13 +35,21 @@ export class FlightEditComponent implements OnChanges, OnInit {
   @Input() flight = initialFlight;
   @Input({ required: true, transform: numberAttribute }) id!: number;
 
-  protected editForm = this.formBuilder.group({
-    id: [0],
-    from: [''],
-    to: [''],
-    date: [new Date().toISOString()],
-    delayed: [false],
+  protected editForm = new FormGroup<FlightForm>({
+    id: new FormControl(0, { validators: [], nonNullable: true }),
+    from: new FormControl('', { nonNullable: true }),
+    to: new FormControl('', { nonNullable: true }),
+    date: new FormControl('', { nonNullable: true }),
+    delayed: new FormControl(false, { nonNullable: true }),
   });
+
+  // protected editForm = this.formBuilder.group({
+  //   id: [0],
+  //   from: [''],
+  //   to: [''],
+  //   date: [new Date().toISOString()],
+  //   delayed: [false],
+  // });
 
   constructor(
     private store: Store,
